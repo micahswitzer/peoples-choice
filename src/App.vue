fdea324d9f813d01a015904b2909ad5bf7e1bc10
Micah Switzer <micah.switzer.dev@outlook.com>
2019-03-26 18:11:03 -0400

    Created top nav skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Header/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Header,
   },
 })
 export default class App extends Vue {}
@@ -19,11 +18,6 @@ export default class App extends Vue {}
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="uk-navbar-container" uk-navbar="mode: click">
+        <div class="uk-navbar-left">
+            <div class="uk-navbar-item uk-logo">
+                People's Choice Awards {{ title }}
+            </div>
+        </div>
+        <div class="uk-navbar-center">
+            <ul class="uk-navbar-nav">
+                <li>
+                    <a href="#">Section 1</a>
+                    <div class="uk-navbar-dropdown">
+                        <ul class="uk-nav uk-navbar-dropdown-nav">
+                            <li class="uk-active"><a href="#">Section 1</a></li>
+                            <li><a href="#">Section 2</a></li>
+                            <li><a href="#">Section 3</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="uk-navbar-right">
+            <ul class="uk-navbar-nav">
+                <li>
+                    <a href="#">Micah Switzer</a>
+                    <div class="uk-navbar-dropdown">
+                        <ul class="uk-nav uk-navbar-dropdown-nav">
+                            <li class="uk-active"><a href="#">Active</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Header extends Vue {
+  @Prop() private title!: string;
+}
+</script>
+
+<style scoped>
+h2::before {
+    content: ' / ';
+}
+</style>
