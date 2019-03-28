94c5ec65206ee288cb839af8b61bbd1e73950b1a
Micah Switzer <micah.switzer.dev@outlook.com>
2019-03-28 14:20:47 -0400

    Began adding project card component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
     <Header/>
+    <ProjectCard title="Project 1"/>
+    <ProjectCard title="Project 2"/>
+    <ProjectCard title="Project 3"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Header from './components/Header.vue';
+import ProjectCard from './components/ProjectCard.vue';
 
 @Component({
   components: {
     Header,
+    ProjectCard,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+
+}
 </script>
 
 <style>
-#app {
-  
-}
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="pc-project">
+		<span>{{ title }}</span>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ProjectCard extends Vue {
+  @Prop() private title!: string;
+  @Prop() private isOpen!: boolean;
+}
+</script>
+
+<style>
+
+</style>
