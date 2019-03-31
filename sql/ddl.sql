fe361ff1c43bc06ff2428623f8cb09432261a0f2
vasterton <zdkemp@cedarville.edu>
2019-03-31 19:41:53 -0400

    Update ddl.sql
    order dude

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,10 +1,10 @@
---section
+-- section
 create table mzpc_section(
 	id int not null auto_increment,
 	primary key (id)
 );
 
---project
+-- project
 create table mzpc_project(
 	id int auto_increment,
 	name varchar(25),
@@ -14,7 +14,17 @@ create table mzpc_project(
 	foreign key (section_id) references mzpc_section(id)
 );
 
---team
+-- user
+create table mzpc_user(
+	id int auto_increment,
+	name varchar(40),
+	pass varchar(25),
+	student boolean,
+	admin boolean,
+	primary key (id)
+);
+
+-- team
 create table mzpc_team(
 	id int auto_increment,
 	name varchar(25),
@@ -23,7 +33,7 @@ create table mzpc_team(
 	foreign key (project_id) references mzpc_project(id)
 );
 
---write_in
+-- write_in
 create table mzpc_write_in(
 	id int auto_increment,
 	message varchar(150),
@@ -34,17 +44,7 @@ create table mzpc_write_in(
 	foreign key (team_id) references mzpc_team(id)
 );
 
---user
-create table mzpc_user(
-	id int auto_increment,
-	name varchar(40),
-	pass varchar(25),
-	student boolean,
-	admin boolean,
-	primary key (id)
-);
-
---score
+-- score
 create table mzpc_score(
 	id int auto_increment,
 	user_id int not null,
@@ -54,7 +54,7 @@ create table mzpc_score(
 	foreign key (team_id) references mzpc_team(id)
 );
 
---member
+-- member
 create table mzpc_member(
 	user_id int not null,
 	team_id int not null,
